1a6d1c734c15f666fe60bfc74792a1067a4f3be3
Lzu Tao <taolzu@gmail.com>
2020-09-25 19:28:44 +0700

    sock

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::io::Write;
 use std::net::TcpStream;
+use std::net::ToSocketAddrs;
 
 fn main() {
     let mut a = Vec::new();
+    let saddrs = ("example.com", 80)
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap();
     for i in 0..300 {
         println!("{}", i);
 
-        let mut stream = match TcpStream::connect(("example.com", 80)) {
+        let mut stream = match TcpStream::connect(saddrs) {
             Ok(s) => s,
             Err(e) => {
                 panic!("raw: {:?}, kind: {:?}", e.raw_os_error(), e.kind());
